1b490c9756be18db1840523de2ccbd2a2dabd5a5
navzlet <sasha_zakirov_2014@mail.ru>
2023-05-11 16:33:43 +0500

    fixed selection of a new items

--- src/store.js
+++ src/store.js
@@ -45,7 +45,7 @@ class Store {
   addItem() {
     this.setState({
       ...this.state,
-      list: [...this.state.list, {code: this.generateIndex(), title: 'Новая запись'}]
+      list: [...this.state.list, {code: this.generateIndex(), title: 'Новая запись', selectionsCount: 0}]
     })
     
   };
